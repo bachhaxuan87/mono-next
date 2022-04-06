847b020a60ef5b514bf2cac51b121b57adede033
Bach Ha <bachhaxuan87@gmail.com>
2022-04-06 16:24:46 +0700

    dddd

--- apps/api/pages/api/account/[[...params]].ts
+++ apps/api/pages/api/account/[[...params]].ts
@@ -4,6 +4,10 @@ import { AuthGuard } from '@api/middlewares';
 import { SignInRequest, UpdateProfileRequest } from '@libs/models';
 import { UserId } from '@api/decorators';
 
+function delay(ms: number) {
+  return new Promise( resolve => setTimeout(resolve, ms) );
+}
+
 class AccountHandler {
   @Post('/sign-in')
   async signIn(@Body() request: SignInRequest) {
@@ -18,7 +22,7 @@ class AccountHandler {
 
   @Get('/:id')
   async getById(@Param('id') id: string) {
-    return AccountService.getById(id);
+    await delay(+id);
   }
 }
 
